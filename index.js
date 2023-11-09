9faf9a997095287f3bc9d15fa6d5068b55b204d1
kgarcia7 <kgarcia7814@gmail.com>
2023-11-09 16:03:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,6 @@ const inquirer = require('inquirer');
 
 const { circle, triangle, square } = require('./lib/shapes');
 
-function writeToFile(fileName, answers) {
-    let svgString = "",
-    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-    svgString += "<g>";
-    svgString += `{answers.shape}`;
-
-    let shapeChoice;
-    if (answers.shape === "circle") {
-        shapeChoice = new circle();
-        svgString += `<circle cx="150" cy="125" r="80" fill="${answers.shapeBackgroundColor}"/>`;
-    } else if (answers.shape === "triangle") {
-        shapeChoice = new triangle();
-        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
-    } else (answers.shape === "square") {
-        shapeChoice = new square();
-        svgString += `<rect x="60" y="10" rx="10" ry="10" width="30" height="30" fill="${answers.shapeBackgroundColor}"/>`;
-    }
-}
-
 
 function promptUser(){
     inquirer
@@ -55,6 +36,33 @@ function promptUser(){
         } else {
             writeToFile("logo.svg", answers);
         }
+    })
+}
+
+function writeToFile(fileName, answers) {
+    let svgString = "";
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgString += "<g>";
+    svgString += `{answers.shape}`;
+
+    let shapeChoice;
+    if (answers.shape === "circle") {
+        shapeChoice = new circle();
+        svgString += `<circle cx="150" cy="125" r="60" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "triangle") {
+        shapeChoice = new triangle();
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "square") {
+        shapeChoice = new square();
+        svgString += `<rect x="60" y="10" rx="10" ry="10" width="30" height="30" fill="${answers.shapeBackgroundColor}"/>`;
+    }
+
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="30" fill=${answers.textColor}">${answers.text}</text>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Generate logo.svg");
     })
 }
 
