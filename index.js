f034c3412de6ac70eb30fd4c009f320fd9fb4b5f
kgarcia7 <kgarcia7814@gmail.com>
2023-11-08 23:28:22 -0500

    Importing inquirer and setting up svg XML

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const { circle, triangle, square } = require('./lib/shapes');
+
+function writeToFile(fileName, answers) {
+    let svgText = "",
+    svgText = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgText += "<g>",
+    svgString += `{answers.shape}`
+}
+
 
