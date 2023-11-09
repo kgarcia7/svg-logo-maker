c9a9f6f6c2449361397ed4baf91300dd24665a3b
kgarcia7 <kgarcia7814@gmail.com>
2023-11-09 15:35:45 -0500

    Adding promptUser function

--- index.js
+++ index.js
@@ -16,10 +16,46 @@ function writeToFile(fileName, answers) {
     } else if (answers.shape === "triangle") {
         shapeChoice = new triangle();
         svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
-    } else (answers.shape === "square"){
+    } else (answers.shape === "square") {
         shapeChoice = new square();
         svgString += `<rect x="60" y="10" rx="10" ry="10" width="30" height="30" fill="${answers.shapeBackgroundColor}"/>`;
     }
 }
 
 
+function promptUser(){
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "text",
+            message: "Enter up to three characters for your logo:"
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "Enter your desired text color (keyword or hexadecimal number):"
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "Choose the shape for your desired logo:",
+            choices: ["circle", "triangle", "square"]
+        },
+        {
+            type: "input",
+            name: "shapeBackgroundColor",
+            message: "Choose the color for your shape (keyword or hexadecimal number):"
+        },
+    ])
+    .then((answers)=> {
+        if (answers.text.length > 3) {
+            console.log("Please enter a value of 3 characters or less");
+            promptUser();
+        } else {
+            writeToFile("logo.svg", answers);
+        }
+    })
+}
+
+promptUser();
